e9f6752afe02365785d94080a7617ee5b6342203
Jason <jasoncrowtherpc@gmail.com>
2019-03-15 12:50:54 +0100

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './resources/js/index.js',
   output: {
     path: path.resolve(__dirname, 'assets/js'),
     filename: 'main.js'
+  },
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['@babel/preset-env']
+        }
+      }
+    ]
   }
 };
